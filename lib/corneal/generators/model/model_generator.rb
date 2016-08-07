4c40825003d4e88b58c7109b2958ba853e010d02
azemoh <joshazemoh@gmail.com>
2016-08-07 04:39:40 +0100

    Generate model create migration

--- lib/corneal/generators/model/model_generator.rb
+++ lib/corneal/generators/model/model_generator.rb
@@ -1,9 +1,11 @@
 require "thor/group"
+require 'active_support/inflector'
 
 module Corneal
   module Generators
     class ModelGenerator < Thor::Group
       include Thor::Actions
+      attr_reader :file_name, :class_name, :model_name, :migration_name, :migration_class_name, :table_name
 
       desc "Generate an ActiveRecord model"
       argument :name, :type => :string, :desc => "Name of the model"
@@ -14,16 +16,34 @@ module Corneal
       end
 
       def setup
-        @class_name = name.camel_case
-        @file_name = name.file_name
+        @model_name           = name.singularize
+        @class_name           = model_name.camel_case
+        @file_name            = model_name.file_name
+        @table_name           = @file_name.pluralize
+        @migration_name       = "create_#{@table_name}"
+        @migration_class_name = @migration_name.camel_case
       end
 
       def create_model
-        template 'model.rb.erb', File.join('app/models', "#{@file_name}.rb")
+        unless model_name == name
+          say "[WARNING] The model name '#{name}' was recognized as a plural, using the singular '#{model_name}' instead."
+        end
+
+        template "model.rb.erb", File.join("app/models", "#{file_name}.rb")
       end
 
-      # def create_migration
-      # end
+      def create_migration
+        migration_files = Dir.entries("db/migrate").select { |path| !File.directory? path }
+
+        if duplicate = migration_files.find { |file| file.include?(migration_name) }
+          say_status :identical, "db/migrate/#{duplicate}", :blue
+        else
+          version = Time.now.utc.strftime("%Y%m%d%H%M%S")
+          migration_file_name = "#{version}_#{migration_name}.rb"
+
+          template "migration.rb.erb", File.join("db/migrate", migration_file_name)
+        end
+      end
     end
   end
 end
