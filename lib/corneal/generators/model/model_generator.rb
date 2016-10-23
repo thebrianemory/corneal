01c9f433e9b89836b759f5b6b049bcd045d6223a
Joshua Azemoh <joshazemoh@gmail.com>
2016-10-23 14:01:06 +0100

    use active_support string helper

--- lib/corneal/generators/model/model_generator.rb
+++ lib/corneal/generators/model/model_generator.rb
@@ -1,5 +1,5 @@
 require "thor/group"
-require 'active_support/inflector'
+require "active_support/inflector"
 
 module Corneal
   module Generators
@@ -8,9 +8,10 @@ module Corneal
       attr_reader :file_name, :class_name, :model_name, :migration_name, :migration_class_name, :table_name
 
       desc "Generate an ActiveRecord model"
-      argument :name, :type => :string, :desc => "Name of the model"
+      argument :name, type: :string, desc: "Name of the model"
       argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
+      # --no-migration make migrations optional
       class_option :migration, type: :boolean, default: true, desc: "Generate migration for model"
 
       def self.source_root
@@ -20,14 +21,14 @@ module Corneal
       def setup
         @model_name           = name.singularize
         @class_name           = model_name.camel_case
-        @file_name             = model_name.file_name
-        @table_name           = @file_name.pluralize
-        @migration_name       = "create_#{@table_name}"
-        @migration_class_name = @migration_name.camel_case
+        @file_name            = model_name.underscore
+        @table_name           = file_name.pluralize
+        @migration_name       = "create_#{table_name}"
+        @migration_class_name = migration_name.camel_case
 
         attributes.map! do |attribute|
           field = attribute.split(":")
-          { name: field[0], type: (field[1] || "string") }
+          { name: field[0], type: field[1] || "string" }
         end
       end
 
