fa6464b45f8433c71c368e3ae29bf34614a9d9ca
Juan Vasquez Gonzalez <juan@logicalbricks.com>
2021-02-23 08:24:40 -0600

    add CLI rspec test

--- spec/corneal/cli_spec.rb
+++ spec/corneal/cli_spec.rb
@@ -1,15 +1,16 @@
-require 'fakefs'
-require_relative "../spec_helper"
 require_relative "../../lib/corneal"
 
 describe Corneal::VERSION do
   subject { Corneal::Generators::AppGenerator }
 
   describe "source_root" do
+    let(:pwd) { File.dirname(__FILE__) }
+    let(:template_path) do
+      pwd.sub "spec/corneal", "lib/corneal/generators/app/templates"
+    end
+
     it "should return with a full path" do
-      pwd           = File.dirname(__FILE__)
-      template_path = pwd.sub 'spec/corneal', 'lib/corneal/generators/app/templates'
-      subject.source_root.must_equal template_path
+      expect(subject.source_root).to eq template_path
     end
   end
 end
